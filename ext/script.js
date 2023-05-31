29e57d45c8ef584d5699213c9448632b9baf8c37
Nikolay Rozhkov <rozhkov@bravy.com>
2023-05-31 19:44:28 +0300

    Wip

--- ext/script.js
+++ ext/script.js
@@ -1,24 +1,4 @@
-function fillInTargetForm() {
-    // Function to fill in the form fields
-    var nameInput = document.getElementById('name');
-    var emailInput = document.getElementById('email');
-    var messageInput = document.getElementById('message');
 
-    // Set the desired values for the form fields
-    nameInput.value = 'John Doe';
-    emailInput.value = 'johndoe@example.com';
-
-    var submitButton = document.querySelector('#myForm button[type="submit"]');
-
-    // Trigger a click event on the submit button
-    var event = new MouseEvent('click', {
-        view: window,
-        bubbles: true,
-        cancelable: true
-    });
-    submitButton.dispatchEvent(event);
-
-}
 
 document.addEventListener("DOMContentLoaded", function () {
     let fillInButton = document.getElementById("fillInButton");
