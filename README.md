360e7599fe5ca09e72588a8335c1559cba3aa429
Nikolay Rozhkov <rozhkov@bravy.com>
2023-05-31 19:12:21 +0300

    Working

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # СТРАХОВОЙ БРОКЕР
 
-ext - расширение для браузера
-form - то что заполняет брокер руками
-target - пример целевой формы
+* ext - расширение для браузера
+* form - то что заполняет брокер руками
+* target - пример целевой формы
 
 
+---
 
-    // "web_accessible_resources": [
-    //     {
-    //         "resources": [
-    //             "styles.css"
-    //         ],
-    //         "matches": ["https://localhost/extension"]
-    //     }
-    // ],
+Чтобы в расширение подгрузить ресурсы из интернета
+
+```json
+"web_accessible_resources": [
+    {
+        "resources": [
+            "styles.css"
+        ],
+        "matches": ["https://localhost/extension"]
+    }
+],
+```
--- ext/script.js
+++ ext/script.js
@@ -1,12 +1,54 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let scrapeButton = document.getElementById("scrapeButton");
-    scrapeButton.addEventListener("click", function () {
-        // Perform the scanning functionality here
-        scanCurrentPage();
-    }, { passive: true });
+function fillInTargetForm() {
+    // Function to fill in the form fields
+    var nameInput = document.getElementById('name');
+    var emailInput = document.getElementById('email');
+    var messageInput = document.getElementById('message');
 
-    function scanCurrentPage() {
-        // Add your code here to scan the current page
-        console.log("Scanning the current page...");
-    }
+    // Set the desired values for the form fields
+    nameInput.value = 'John Doe';
+    emailInput.value = 'johndoe@example.com';
+
+    var submitButton = document.querySelector('#myForm button[type="submit"]');
+
+    // Trigger a click event on the submit button
+    var event = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    });
+    submitButton.dispatchEvent(event);
+
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    let fillInButton = document.getElementById("fillInButton");
+    fillInButton.addEventListener("click", function () {
+        // Perform the scanning functionality here
+
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            let tab = tabs[0];
+
+            if(!tab) return;
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                func: () => {
+                    function onTabReady() {
+                        // Your code here
+                        fillInTargetForm();
+                    }
+
+                    // Check if the document is ready
+                    if (document.readyState === 'complete') {
+                        // If the document is already loaded, execute the function immediately
+                        onTabReady();
+                    } else {
+                        // Otherwise, wait for the 'DOMContentLoaded' event to execute the function
+                        document.addEventListener('DOMContentLoaded', onTabReady);
+                    }
+                }
+            });
+        });
+
+    }, { passive: true });
 });
+
